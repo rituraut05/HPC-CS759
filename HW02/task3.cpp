ff7917d04408c22ddef1e6374d4c0a5d2c9d221a
Ritu Raut <rituraut05@Ritus-Air.lan>
2022-09-20 19:00:50 -0500

    updated code to delete result array

--- HW02/task3.cpp
+++ HW02/task3.cpp
@@ -31,24 +31,34 @@ int main(){
         vecB.push_back(random2);
     }
     cout<<n<<endl;
+
     start = high_resolution_clock::now();
     mmul1(A,B,C,n);
     end = high_resolution_clock::now();
     duration_sec = std::chrono::duration_cast<duration<double, std::milli>>(end - start);
     cout<<duration_sec.count()<<endl;
     cout<<C[n*n-1]<<endl;
+    delete[] C;
+    C= new double[n*n];
+
     start = high_resolution_clock::now();
     mmul2(A,B,C,n);
     end = high_resolution_clock::now();
     duration_sec = std::chrono::duration_cast<duration<double, std::milli>>(end - start);
     cout<<duration_sec.count()<<endl;
     cout<<C[n*n-1]<<endl;
+    delete[] C;
+    C= new double[n*n];
+
     start = high_resolution_clock::now();
     mmul3(A,B,C,n);
     end = high_resolution_clock::now();
     duration_sec = std::chrono::duration_cast<duration<double, std::milli>>(end - start);
     cout<<duration_sec.count()<<endl;
     cout<<C[n*n-1]<<endl;
+    delete[] C;
+    C= new double[n*n];
+
     start = high_resolution_clock::now();
     mmul4(vecA,vecB,C,n);
     end = high_resolution_clock::now();
