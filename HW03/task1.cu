b3ddc2f38ee27593e783b971c0f168c82fb1a756
Ritu Raut <rituraut05@Ritus-Air.lan>
2022-09-30 15:16:05 -0500

    Added comments

--- HW03/task1.cu
+++ HW03/task1.cu
@@ -1,8 +1,11 @@
 #include<iostream>
 using namespace std;
+
 /*
-Write a C++ program using CUDA in a file calledtask1.cuwhich computes the factorial of integersfrom 1 to 8, by launching a GPU kernel with 1 block and 8 threads.  Inside the kernel, each threadshould usestd::printfto write outa!=b(followed by a newline), whereais one of the 8 integers,andbis the result ofa!.  (Follow your kernel call with a call tocudaDeviceSynchronize()so thatthe host waits for the kernel to finish printing before returning frommain.)•Compile:nvcc task1.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std=c++17 -otask1•Run (on Euler, use Slurm sbatch!):./task1•Expected output (showing only 4 out of the 8 lines expected; lines could be out of order):1!=12!=23!=64!=24
+Q1. Write a C++ program using CUDA in a file calledtask1.cuwhich computes the factorial of integersfrom 1 to 8, by launching a GPU kernel with 1 block and 8 threads.  Inside the kernel, each threadshould usestd::printfto write outa!=b(followed by a newline), whereais one of the 8 integers,andbis the result ofa!.  (Follow your kernel call with a call tocudaDeviceSynchronize()so thatthe host waits for the kernel to finish printing before returning frommain.)•Compile:nvcc task1.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std=c++17 -otask1•Run (on Euler, use Slurm sbatch!):./task1•Expected output (showing only 4 out of the 8 lines expected; lines could be out of order):1!=12!=23!=64!=24
 */
+
+// Kernel function to find the factorial of the given number
 __global__ 
 void factorial(){
     int tid = threadIdx.x;
@@ -12,9 +15,14 @@ void factorial(){
     }
     printf("%d!=%d\n", tid+1, fact);
 }
+
 int main(){
     int n = 8;
+
+    // Launch kernel on 1 block with 8 threads
     factorial<<<1, n>>>();
+
+    // Wait for GPU to finish before accessing on host
     cudaDeviceSynchronize();
     return 0;
 }
