39dca95c46a709532df9e7a0fbfa7ffcf728c133
RITU RAVINDRA RAUT <rraut2@euler-login-1.wacc.wisc.edu>
2022-10-14 11:51:21 -0500

    Added code for reduce

---
+++ HW05/reduce.cu
@@ -0,0 +1,73 @@
+//Q1.a) Implement in a file called reduce.cu the functions reduce and reduce kernel as de-
+// clared and described in reduce.cuh. Your reduce kernel should use the alteration
+// from Reduction #4 (“First Add During Load” from Lecture 15). The reduce kernel
+// function should be called inside the reduce function (repeatedly if needed) until the
+// final sum of the entire array is obtained.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <cuda.h>
+#include "reduce.cuh"
+
+// implements the 'first add during global load' version (Kernel 4) for the parallel reduction
+// g_idata is the array to be reduced, and is available on the device.
+// g_odata is the array that the reduced results will be written to, and is available on the device.
+// expects a 1D configuration.
+// uses only dynamically allocated shared memory.
+__global__ void reduce_kernel(float* g_idata, float* g_odata, unsigned int n) {
+    extern __shared__ float shMemArray[]; // dynamically allocated shared memory
+    unsigned int tid = threadIdx.x; // thread ID
+    unsigned int bid = blockIdx.x; // block ID
+    unsigned int bdim = blockDim.x; // block dimension
+
+    // first index of element for sum reduction
+    unsigned int i = bid * (bdim*2) + tid; 
+    // reduced 2 elements per thread
+    shMemArray[tid] = g_idata[i] + g_idata[i + bdim];
+
+    __syncthreads(); // wait for all threads to finish
+
+    // reduction in shared memory
+    for (unsigned int s = bdim / 2; s > 0; s >>= 1) {
+        if (tid < s) {
+            shMemArray[tid] += shMemArray[tid + s];
+        }
+        __syncthreads(); // wait for all threads to finish
+    }
+
+    // write result for this block to global memory
+    if (tid == 0) {
+        g_odata[bid] = shMemArray[0];
+    }
+}
+
+// the sum of all elements in the *input array should be written to the first element of the *input array.
+// calls reduce_kernel repeatedly if needed. _No part_ of the sum should be computed on host.
+// *input is an array of length N in device memory.
+// *output is an array of length = (number of blocks needed for the first call of the reduce_kernel) in device memory.
+// configures the kernel calls using threads_per_block threads per block.
+// the function should end in a call to cudaDeviceSynchronize for timing purposes
+__host__ void reduce(float** input, float** output, unsigned int N, unsigned int threads_per_block) {
+
+    while(N > 1) {
+        unsigned int blocks=1; // 
+        if (threads_per_block < N){
+            blocks = (N + threads_per_block - 1) / (2 * threads_per_block);
+        }
+
+        // call reduce_kernel
+        reduce_kernel<<<blocks, threads_per_block, threads_per_block * sizeof(float)>>>(*input, *output, N);
+        N = blocks;
+
+        // swap input and output for next iteration
+        float* temp;
+        temp = *output;
+        *output = *input;
+        *input = temp;
+    }
+    
+    // wait for all threads to finish
+    cudaDeviceSynchronize();
+
+}
---
+++ HW05/task1.cu
@@ -0,0 +1,77 @@
+// //Q1.b) Write a test program task1.cu which will complete the following (some memory man-
+// // agement steps are omitted for clarity, but you should implement them in your code):
+// // •Create and fill an array of length N with random numbers in the range [-1,1] on the
+// // host, where N is the first command line argument as below.
+// // •Copy this host array to device as the input array where the reduction will be
+// // performed on.
+// // •Create another output array on the device that has its length equal to the number
+// // of blocks required for the first call to the kernel function reduce kernel.
+// // •Call your reduce function to sum all the elements in the input array, with the
+// // threads per block read from the second command line argument as below.
+// // •Print the resulting sum.
+// // •Print the time taken to run the reduce function in milliseconds.
+// // •Compile: nvcc task1.cu reduce.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas
+// // -O3 -std c++17 -o task1
+// // •Run (where N ≤ 230 and threads per block are positive integers, and N is not
+// // necessarily a power of 2):
+// // ./task1 N threads per block
+
+
+#include <iostream>
+#include <random>
+#include "reduce.cuh"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    unsigned int n = atoi(argv[1]); // number of elements in the array
+    unsigned int threadsPerBlock = atoi(argv[2]); // number of threads per block
+    unsigned int blocksPerGrid = (n + threadsPerBlock - 1) / threadsPerBlock; // number of blocks per grid
+        
+        float *h_input = new float[n]; // host input array
+        float *d_input, *d_output; // device input and output arrays
+
+        // allocate memory on the device
+        cudaMalloc((void**)&d_input, n * sizeof(float));
+        cudaMalloc((void**)&d_output, blocksPerGrid * sizeof(float));
+
+        // fill the host input array with random numbers
+        random_device rd;
+        mt19937 gen(rd());
+        uniform_real_distribution<> dis(-1, 1);
+        for (int i = 0; i < n; i++) {
+            // h_input[i] = dis(gen);
+            h_input[i] = 1;
+        }
+
+        // copy the host input array to the device
+        cudaMemcpy(d_input, h_input, n * sizeof(float), cudaMemcpyHostToDevice);
+        cudaMemset(d_output, 0, blocksPerGrid * sizeof(float));
+        
+        cudaEvent_t start, stop; // events for timing
+        cudaEventCreate(&start); // create start event
+        cudaEventCreate(&stop); // create stop event
+        cudaEventRecord(start, 0); // record start event
+        reduce(&d_input, &d_output, n, threadsPerBlock); // call reduce function
+        cudaEventRecord(stop, 0); // record stop event
+        cudaEventSynchronize(stop); // wait for stop event to complete
+        float elapsedTime; // time in milliseconds
+        cudaEventElapsedTime(&elapsedTime, start, stop); // compute elapsed time
+        cudaEventDestroy(start); // destroy start event
+        cudaEventDestroy(stop); // destroy stop event
+        
+        float res;
+        cudaMemcpy(&res, d_input, blocksPerGrid * sizeof(float), cudaMemcpyDeviceToHost); // copy result from device to host
+        printf("%f\n", res); // print result
+        printf("%f\n", elapsedTime); // print time taken in milliseconds
+
+        // free memory on the device
+        cudaFree(d_input);
+        cudaFree(d_output);
+
+        // free memory on the host
+        delete[] h_input;
+        return 0;
+
+  return 0;
+}
