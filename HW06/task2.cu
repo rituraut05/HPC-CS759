4fd2a4b6ff69ee54531e852cbfbc46fd90a73ab2
RITU RAVINDRA RAUT <rraut2@euler-login-2.wacc.wisc.edu>
2022-10-21 20:31:08 -0500

    Added task2.cu

--- HW06/task2.cu
+++ HW06/task2.cu
@@ -24,47 +24,37 @@ int main(int argc, char *argv[])
     unsigned int n = atoi(argv[1]); //size of array
     unsigned int threads_per_block = atoi(argv[2]); //threads per block
     
-    float *input = new float[n]; //input array
-    float *output = new float[n]; //output array
+    float *deviceInput, *deviceOutput; //device arrays
 
-    // random number generator
+    //allocate memory on device
+    cudaMallocManaged((void **)&deviceInput, n * sizeof(float));
+    cudaMallocManaged((void **)&deviceOutput, n * sizeof(float));
+
+    
+    //generating random number.
     random_device rd;
     mt19937 gen(rd());
-    uniform_real_distribution<> disFloat(-1.0, 1.0);
-
-    //fill array with random numbers
+    uniform_real_distribution<> dis(-1.0, 1.0);
     for (unsigned int i = 0; i < n; i++){
-        input[i] = disFloat(gen);
-        output[i]=disFloat(gen);
-    }
+        deviceInput[i] = dis(gen);
+        deviceOutput[i]=dis(gen);
+    }    
 
-    float *deviceInput, *deviceOutput;
-    cudaMalloc(&deviceInput, n  * sizeof(float)); //allocate memory on device for input array
-    cudaMalloc(&deviceOutput, n  * sizeof(float)); //allocate memory on device for output array
-
-    cudaMemcpy(deviceInput, input, n*  sizeof(float), cudaMemcpyHostToDevice); //copy input array to device
-    cudaMemcpy(deviceOutput, output, n * sizeof(float), cudaMemcpyHostToDevice); //copy output array to device
-    
-     // Create CUDA events
-    cudaEvent_t start, stop; 
-    cudaEventCreate(&start); // create start event
-    cudaEventCreate(&stop); // create stop event
-    cudaEventRecord(start); // record start event
+    cudaEvent_t start, stop; //start and stop events
+    cudaEventCreate(&start); //create start event
+    cudaEventCreate(&stop); //create stop event
+    cudaEventRecord(start); //record start event
     scan(deviceInput,deviceOutput,n,threads_per_block); //call scan function
-    cudaEventRecord(stop, 0); // record stop event
-    cudaEventSynchronize(stop); // wait for stop event to complete
-    cudaDeviceSynchronize(); // wait for device to finish
-    cudaEventDestroy(start); // destroy start event
-    cudaEventDestroy(stop); // destroy stop event
-    cudaMemcpy(output, deviceOutput, n * sizeof(float), cudaMemcpyDeviceToHost); //copy output array from device to host
-    cout << output[n-1] << endl; //print last element of output array
+    cudaEventRecord(stop, 0); //record stop event
+    cudaEventSynchronize(stop); //wait for stop event to complete
+    cout<<deviceOutput[n-1]<<endl; //print last element of output array
     float elapsedTime;
     cudaEventElapsedTime(&elapsedTime, start, stop); //calculate elapsed time
-    cout << elapsedTime << endl; //print elapsed time
+    cout<<elapsedTime<<endl; //print elapsed time
+    cudaEventDestroy(start); //destroy start event
+    cudaEventDestroy(stop); //destroy stop event
     cudaFree(deviceInput); //free device memory
     cudaFree(deviceOutput); //free device memory
-    delete[] input; //free host memory
-    delete[] output; //free host memory
     return 0;
 }
 
