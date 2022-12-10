0018dd7d41fa07996cc8e808f796f81e14a325c4
RITU RAVINDRA RAUT <rraut2@euler-login-2.wacc.wisc.edu>
2022-12-10 17:09:58 -0600

    Added helper functions under __device__

---
+++ FinalProject/helper.cuh
@@ -0,0 +1,22 @@
+#ifndef HELPER_CUH
+#define HELPER_CUH
+
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+#define N 9
+#define N2 81
+#define Nr 3
+
+void read_file(string filename, int* board);
+void print_sudoku(int* board);
+__device__ bool check_row(int* &sudoku, int row, int num);
+__device__ bool check_col(int* &sudoku, int col, int num);
+__device__ bool check_box(int* &sudoku, int row, int col, int num);
+__device__ bool check(int* &sudoku, int row, int col, int num);
+__device__ bool find_empty(int* &sudoku, int &index);
+
+#endif
