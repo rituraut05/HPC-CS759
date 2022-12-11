f1703cad01de4f7559ffa1ee4067094c7e2addd2
RITU RAVINDRA RAUT <rraut2@euler-login-2.wacc.wisc.edu>
2022-12-10 19:46:23 -0600

    Added a helper function

--- FinalProject/helper.cuh
+++ FinalProject/helper.cuh
@@ -13,6 +13,7 @@ using namespace std;
 
 void read_file(string filename, int* board);
 void print_sudoku(int* board);
+void write_file(string filename, int* board);
 __device__ bool check_row(int* &sudoku, int row, int num);
 __device__ bool check_col(int* &sudoku, int col, int num);
 __device__ bool check_box(int* &sudoku, int row, int col, int num);
